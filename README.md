42f98c7ce8ebd7906aada38a2023229e7782c344
Abner Silva <link_abner.p.s@hotmail.com>
2020-04-21 21:35:43 -0300

    Iniciado projeto com design de tela

---
+++ README.md
@@ -0,0 +1,33 @@
+<h4 align="center">
+<img src="./assets/splash.png" style="border-radius: 10px"/>
+<br>Calculador Moderna - Neumorphism Design
+</h4>
+<p align="center">
+    <img src="https://img.shields.io/badge/Version-0.1-blue.svg">
+</p>
+
+## :bulb: Objetivo do projeto
+
+Calcula com funcionalidades simples, porém com um designer moderno utilizando técnicas de neumorphism.
+
+## :computer: Técnologias
+
+- [Neumorphism Shadows](https://github.com/tokkozhin/react-native-neomorph-shadows)
+
+## :iphone: Design de tela
+
+<img src="./assets/tela.png" style="border-radius: 5px">
+
+ ## :tv: Redes Sociais
+ 
+   <a href="https://github.com/AbnerPS" target="_blank" >
+    <img alt="Github" src="https://img.shields.io/badge/Github--%23F8952D?style=social&logo=github"></a> 
+  
+  <a href="https://www.instagram.com/abner.p.s/" target="_blank" >
+    <img alt="Instagram" src="https://img.shields.io/badge/Instagram--%23F8952D?style=social&logo=instagram"></a> 
+  
+  <a href="https://www.facebook.com/AbnerGuthiwill" target="_blank" >
+    <img alt="Facebook" src="https://img.shields.io/badge/Facebook--%23F8952D?style=social&logo=facebook"></a> 
+
+  <a href="https://www.linkedin.com/in/abner-pereira-silva-8715a326/" target="_blank" >
+    <img alt="Linkedin" src="https://img.shields.io/badge/Linkedin--%23F8952D?style=social&logo=linkedin"></a>
---
+++ styles.js
@@ -0,0 +1,155 @@
+import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants'
+
+export default StyleSheet.create({
+    container: {
+        backgroundColor: "#F2F3F7",
+        flex: 1,
+        paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 10
+      },
+
+      header: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
+      },
+
+      neuMenu: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignContent: "center",
+        flexWrap: "wrap",
+        shadowRadius: 3,
+        borderRadius: 20,
+        backgroundColor: '#F2F3F7',
+        width: 340,
+        height: 80,
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+
+      neuButtonMenu: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        shadowRadius: 7,
+        borderRadius: 50,
+        backgroundColor: '#F2F3F7',
+        width: 55,
+        height: 55,
+      },
+
+      borderButtonMenu: {
+        marginEnd: 15,
+        shadowRadius: 7,
+        borderRadius: 90,
+        backgroundColor: '#F19F9F',
+        width: 65,
+        height: 65,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+
+      title: {
+        fontSize: 20,
+        textTransform: "uppercase",
+      },
+
+      operation: {
+        paddingTop: 40,
+        alignItems:"flex-end"
+      },
+
+      operatingText: {
+        color: "#91a1bd",
+        fontSize: 20
+      },
+
+      resultText: {
+        fontSize: 50
+      },
+
+      buttons: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+        marginTop: 10
+      },
+
+      buttonNumber: {
+        marginTop: 20,
+        marginEnd: 10,
+        shadowRadius: 4,
+        borderRadius: 60,
+        backgroundColor: "#F2F3F7",
+        width: 60,
+        height: 60,
+        justifyContent: "center",
+        alignContent: "center"
+      },
+
+      buttonNumberZero: {
+        marginTop: 20,
+        marginEnd: 10,
+        shadowRadius: 4,
+        borderRadius: 60,
+        backgroundColor: "#F2F3F7",
+        width: 150,
+        height: 60,
+        justifyContent: "center",
+        alignContent: "center"
+      },
+
+        neuButtonOp: {
+          shadowRadius: 7,
+          borderRadius: 50,
+          backgroundColor: '#F19F9F',
+          width: 50,
+          height: 50,
+          justifyContent: 'center',
+          alignItems: 'center'
+        },
+
+          borderButtonOp: {
+            marginEnd: 10,
+            marginTop: 20,
+            shadowRadius: 4,
+            borderRadius: 60,
+            backgroundColor: '#F2F3F7',
+            width: 60,
+            height: 60,
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+
+          neuButtonFunc: {
+            shadowRadius: 7,
+            borderRadius: 50,
+            backgroundColor: '#555',
+            width: 50,
+            height: 50,
+            justifyContent: 'center',
+            alignItems: 'center'
+          },
+
+      numbers: {
+        backgroundColor: "#F2F3F7",
+        alignItems: "center",
+        justifyContent: "center",
+        borderColor: "#e2ecfd",
+        borderWidth: 1,
+    },
+
+    numberText: {
+      fontSize: 30,
+      textAlign: "center"
+    },
+
+    FuncText: {
+      fontSize: 28,
+      textAlign: "center",
+      color: "#F2F3F7"
+    },
+})
